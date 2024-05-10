95e4c06c86108fa9b7cb2dbc8c54f8bdadc0b58f
Omar Gerardo <omargrard@gmail.com>
2024-05-10 22:30:27 +0800

    better launch profiles

--- lib/img/list.dart
+++ lib/img/list.dart
@@ -31,4 +31,12 @@ List data = [
 
     "price": 25,
   },
+  {
+    "imageUrl" : "https://images.unsplash.com/photo-1551024506-0bccd828d307?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "text" : "Dessert",
+    "count" : 0,
+    "max" : 10,
+
+    "price": 25,
+  },
 ];
