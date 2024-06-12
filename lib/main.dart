59bf0446969cd6d1b3c837ddc01581c2d78c747a
inix-x <omargrard@gmail.com>
2024-06-13 02:14:02 +0800

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
-
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/providers/categoryprovider.dart';
 import 'package:flutter_application_1/screens/getstarted.dart';
 import 'package:provider/provider.dart';
 
-
-void main () async{
+void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  runApp(const MyApp()
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: dotenv.env['GOOGLE_API_KEY']!,
+      appId: dotenv.env['APP_ID']!,
+      messagingSenderId: dotenv.env['PROJECT_ID']!,
+      projectId: dotenv.env['PROJECT_ID']!,
+    ),
   );
+  runApp(const MyApp());
 }
-
 
 
 class MyApp extends StatelessWidget {
---
+++ lib/utils/generate_google_services.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main() {
+  final envFile = File('.env');
+  final env = Map.fromEntries(
+    envFile.readAsLinesSync().map((line) {
+      final split = line.split('=');
+      return MapEntry(split[0], split[1]);
+    }),
+  );
+
+  final templateFile = File('lib/utils/google-services-template.json');
+  var template = templateFile.readAsStringSync();
+
+  env.forEach((key, value) {
+    template = template.replaceAll('\${$key}', value);
+  });
+
+  final outputFile = File('android/app/google-services.json');
+  outputFile.writeAsStringSync(template);
+
+  print('google-services.json generated successfully.');
+}
