9a793fde0f7b412109ba7a72d95ccc00ecbb816c
inix-x <omargrard@gmail.com>
2024-06-13 03:26:20 +0800

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,21 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/providers/categoryprovider.dart';
 import 'package:flutter_application_1/screens/getstarted.dart';
 import 'package:provider/provider.dart';
 
+
 void main() async {
-  await dotenv.load(fileName: "../.env");
+  await dotenv.load(fileName: '.env');
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: dotenv.env['GOOGLE_API_KEY']!,
-      appId: dotenv.env['APP_ID']!,
-      messagingSenderId: dotenv.env['PROJECT_ID']!,
-      projectId: dotenv.env['PROJECT_ID']!,
-    ),
+  await Firebase.initializeApp();
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.playIntegrity,
   );
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
